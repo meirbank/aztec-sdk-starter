481b5346e995e761560a26e5a5637d03fd82fcd7
meirbank <meir@meirbank.com>
2022-10-09 07:45:24 -0500

    fully working

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -19,13 +19,392 @@ import {
   BridgeCallData,
   DefiSettlementTime
 } from "@aztec/sdk";
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 import { randomBytes } from "crypto";
-
+import { Center, Square, Circle } from '@chakra-ui/react'
 import {
   depositEthToAztec,
   registerAccount,
 } from "./utils";
+
+
+
+const nftABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_symbol",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "currentTokenId",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "getApproved",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "isApprovedForAll",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "mint",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "setApprovalForAll",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes4",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
 
 const bridgeReadAbi = [
   {
@@ -66,13 +445,42 @@ const Home: NextPage = () => {
   const [alias, setAlias] = useState("");
   const [amount, setAmount] = useState(0);
   const [virtualAssetId, setVirtualAssetId] = useState(0);
+  const [nftPortfolio, setNftPortfolio] = useState<object[]>(
+    []
+  );
+
+
+  const baseuri = "https://bafybeighak7n5qxop5cuz2hcnyqweq6sdsczdahhzhnjqmbt7sm5qecf4y.ipfs.dweb.link/Zkunks-metadata/"
+  async function getNFTData(id){
+    return fetch(`${baseuri}${id}.json`)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  async function loadNFTs(nftIds){
+    const arr = []
+    for (var i =0; i< nftIds.length; i++){
+      const obj = await getNFTData(nftIds[i]);
+      console.log("Obj", obj)
+      arr.push(obj)
+    }
+
+    setNftPortfolio(arr)
+    console.log("NFT PORT", nftPortfolio, arr, typeof nftPortfolio, typeof arr)
+  }
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
       setHasMetamask(true);
     }
+    
     window.ethereum.on("accountsChanged", () => location.reload());
-  });
+  }, []);
 
   async function connect() {
     setConnecting(true);
@@ -147,18 +555,20 @@ const Home: NextPage = () => {
       )
     );
 
+    getBalance();
+
   }
+
 
   async function mint(){
 
-
-    const nftBridge = new BridgeCallData(32, 0, virtualAssetIdPlaceholder, undefined, undefined, 0);
+    const nftBridge = new BridgeCallData(44, 0, virtualAssetIdPlaceholder, undefined, undefined, 0);
 
     let bridge = nftBridge;
 
     const tokenAssetValue: AssetValue = {
       assetId: 0,
-      value: 1n,
+      value: 1000000000000000n,
     };
 
     const fee = (await sdk.getDefiFees(bridge))[DefiSettlementTime.INSTANT];
@@ -233,20 +643,46 @@ const Home: NextPage = () => {
 
     console.log(defiTxs)
 
-    defiTxs.map(async (tx) => {
-      console.log(`Bridge Id: ${tx.bridgeCallData.bridgeAddressId}`)
-      const balance = await sdk.getBalance(accountPublicKey, tx.bridgeCallData.outputAssetIdA)
-      console.log(`Output asset id ${tx.bridgeCallData.outputAssetIdA} balance: ${balance.value}`)
-      setVirtualAssetId(tx.bridgeCallData.outputAssetIdA)
-    })
+    let interactionNonces = [];
+
+
+    for (var i=0; i < defiTxs.length; i++){
+      if (defiTxs[i].interactionResult.interactionNonce){
+        interactionNonces.push(defiTxs[i].interactionResult.interactionNonce);
+      }
+    }
+    console.log("nonces", interactionNonces)
+
+    let realNftIds = [];
+    for (var i=0; i< interactionNonces.length; i++){
+      let myNftId = await mapVirtualAssetToNFT(interactionNonces[i]);
+      console.log("NFT ID Result", myNftId)
+      if (myNftId > 0) {
+        realNftIds.push(myNftId);
+      }
+    }
+    console.log("Nft ids", realNftIds)
+
+    loadNFTs(realNftIds);
   }
 
-  async function mapVirtualAssetToNFT(){
-    const id = virtualAssetId;
-    const bridgeAddress = "0xa6c8113388ef842b2aca1d3ce41ecccbb4a35e2a";
+  async function mapVirtualAssetToNFT(id){
+    const bridgeAddress = "0xb6cd5313407f9930229a64336495121ba5b3a248";
     const bridgeContract = new ethers.Contract(bridgeAddress, bridgeReadAbi, signer);
-    const nftId = await bridgeContract.owners(virtualAssetId);
-    console.log("NFT id", nftId.toNumber());
+    const nftId = await bridgeContract.owners(id);
+    console.log("Mapping for ", id, nftId.toNumber());
+    return nftId.toNumber()
+  }
+
+  async function sanityCheckIfNFTExists(){
+    const nftAddress = "0x2A4B8866C05b087D3779e200aa6f928B4C846A02";
+    const nftContract = new ethers.Contract(nftAddress, nftABI, signer);
+    //const newNFT = await nftContract.mint();
+    // console.log("new ft",  newNFT)
+    const nftId = await nftContract.currentTokenId();
+    const nftname = await nftContract.name();
+    console.log("NFT id sanity check", nftId.toNumber(), nftname);
+
   }
 
   async function getSpendingKey() {
@@ -303,44 +739,28 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      {hasMetamask ? (
-        isConnected ? (
-          "Connected! "
-        ) : (
-          <button onClick={() => connect()}>Connect</button>
-        )
-      ) : (
-        "Please install metamask"
-      )}
-      {connecting ? "Please wait, setting up Aztec" : ""}
       {sdk ? (
-        <div>
+        <Center style={{padding:50, margin:"0 auto", width:"100%"}}>
           {(accountPrivateKey && !account0)? (
-            <button onClick={() => initUsersAndPrintBalances()}>
+            <Button onClick={() => initUsersAndPrintBalances()}>
               initialize
-            </button>
+            </Button>
           ) : (
             ""
           )}
           {(!accountPrivateKey) ? (
-            <button onClick={() => login()}>Login</button>
+            <Button onClick={() => login()}>Login</Button>
           ) : (
             ""
           )}
+
           {spendingSigner && account0 ? (
-            <><button onClick={() => mint()}>
+
+            
+            <Button onClick={() => mint()}>
             Mint
-          </button>
-            <button onClick={() => getBalance()}>
-            Get Balance
-          </button></>
-          ) : (
-            ""
-          )}
-          {(spendingSigner && account0 && virtualAssetId) ? (
-            <button onClick={() => mapVirtualAssetToNFT()}>
-              retrieve NFT ID
-            </button>
+          </Button>
+
           ) : (
             ""
           )}
@@ -359,16 +779,38 @@ const Home: NextPage = () => {
             ""
           )}
           {!spendingSigner && account0 ? (
-            <button onClick={() => getSpendingKey()}>
+            <Button onClick={() => getSpendingKey()}>
               Create Spending Key (Signer)
-            </button>
+            </Button>
           ) : (
             ""
           )}
-        </div>
+        </Center>
       ) : (
         ""
       )}
+      
+      <Center>
+      {hasMetamask ? (
+        isConnected ? (
+          "Connected! "
+        ) :  connecting? ("") : (
+          <Button style={{marginTop:50}} onClick={() => connect()}>Connect</Button>
+        )
+      ) : (
+        "Please install metamask"
+      )}
+      {connecting ? <p style={{marginTop:50}}>Please wait, setting up Aztec</p> : ""}
+      </Center>
+      
+
+<div style={{margin:20}}>
+{(nftPortfolio && nftPortfolio.length > 0) && <Center><Heading>My Portfolio</Heading></Center>}
+<br/>
+<Grid templateColumns='repeat(5, 1fr)' gap={6}>
+{nftPortfolio && nftPortfolio.map((item, i) => (<GridItem w='100%' h='10' bg='blue.500'><img style={{width:400}} src={item.image}/><p><b>Zk</b>unk{item.name}</p></GridItem> )) }
+</Grid>
+</div>
     </div>
   );
 };
